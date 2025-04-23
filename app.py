e5fbb9e39b89a69a1806e5edeba3db6af30a0ab5
ALFreitas-max <profalfreitas@gmail.com>
2025-04-23 16:18:15 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="SGC.IA – Sistema de Gestão por Competências com IA", layout="wide")
+st.title("SGC.IA – Sistema de Gestão por Competências com Inteligência Artificial")
+st.markdown("Protótipo funcional com entrada de dados, análise de competências e exportação.")
